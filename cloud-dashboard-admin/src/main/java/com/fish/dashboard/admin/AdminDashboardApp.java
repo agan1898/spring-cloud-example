9d3475d85cc0785853319d5bc32441cd257ad9d8
agan1898 <15183179442@163.com>
2017-11-05 11:46:57 +0800

    spring-cloud

---
+++ cloud-dashboard-admin/src/main/java/com/fish/dashboard/admin/AdminDashboardApp.java
@@ -0,0 +1,15 @@
+package com.fish.dashboard.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminDashboardApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminDashboardApp.class, args);
+    }
+}
